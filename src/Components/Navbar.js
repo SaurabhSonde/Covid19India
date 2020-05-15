90822c80eec39c56c4350cadb24ce1b0fa1078e7
Saurabh Sonde <saurabhsonde111@gmail.com>
2020-05-15 18:03:21 +0530

    initial commit

---
+++ src/Components/Navbar.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+
+const Navbar = () => {
+	return(
+         
+     <div>
+     
+<nav> 
+<div className="nav-wrapper" style={{backgroundColor: "#ffffff"}}> 
+
+<span className="brand-logo" style={{color: "#586776",fontFamily: "'Balsamiq Sans', cursive"}}>Covid19India
+</span>
+
+ <span data-target="mobile-demo" className="sidenav-trigger">
+ <h5><span role="img" aria-label="Indian flag">🇮🇳</span></h5>
+ </span>
+
+     </div>
+     </nav>
+     </div>
+    
+	
+	)
+}
+
+
+
+export default Navbar;
